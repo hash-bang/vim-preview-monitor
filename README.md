35dd50064e654d2896f3fb7801ac115fd3dbfae4
Matt Carter <m@ttcarter.com>
2020-01-11 17:38:33 +1000

    Added `MonOnLazy` + `MonOff` commands Added docs REFACTOR: Major code tidy

---
+++ README.md
@@ -0,0 +1,68 @@
+vim-preview-monitor
+===================
+NeoVim remote plugin to watch a file and display it in a preview window.
+
+This project came about because I needed a non-intrucive method of displaying compile errors from a complex pipeline without invoking the pipeline via Vi itself.
+
+
+How it works
+------------
+
+1. When enabled on a buffer the directory tree is examined upwards until we hit the `mon_root_file`, this is then used as the project root
+2. Set up a file system monitor for the `mon_file` within the discovered root directory
+3. When the file exists (or has a size above zero) we display the preview window within the tab showing its contents, when the file does not exist we close the preview window.
+
+
+Settings / Variables
+====================
+Settings are applicable per-buffer.
+
+```
+let b:mon_root_file = "package.json"
+let b:mon_file = ".error"
+let b:mon_window_opts = "+wincmd\\ J"
+```
+
+
+`mon_root_file`
+===============
+Defaults to `package.json`.
+The file to look for which identifies a project root.
+
+
+`mon_file`
+==========
+Defaults to `.error`.
+The file to display in the preview window when it is present.
+
+
+`mon_window_opts`
+=================
+Defaults to `+wincmd\\ J`.
+Additional command line options to specify when invoking the monitor command.
+
+
+Commands
+========
+Any of the following commands can be entered per-buffer in Normal Mode to enable monitoring behaviour.
+
+
+MonOn
+-----
+Enable the Monitor for this buffer using local variables to identify the project root / monitor file.
+
+
+MonOnLazy
+---------
+Similar to `MonOn` but fail silently and disable if the buffer path cannot be determined or the project root directory cannot be located.
+
+
+MonOff
+------
+Disable monitoring for this buffer.
+
+
+Troubleshooting
+===============
+NeoVim does not automatically refresh remote plugins.
+If any of the `Mon*` functions are not available try running `::UpdateRemotePlugins` to make NeoVim regenerate its cache.
--- rplugin/node/index.js
+++ rplugin/node/index.js
@@ -7,60 +7,135 @@ var fspath = require('path');
 * @param {NeoVimPlugin} plugin The NeoVimPlugin instance
 */
 module.exports = function(plugin) {
-	// FIXME: These should pick up from g:mon_* but I've no idea how to do that - MC 2020-01-11
-	var projectRoot; // Project root directory where `monitorFile` may exist
-	var projectRootFile = 'package.json';
-	var monitorFile = '.error';
-	var monitorOpts = '+wincmd\\ J';
+	/**
+	* Object tracking each absolute file name and its watcher
+	* @type {Object}
+	* @property {string} path The path of the buffer, also used as the object key
+	* @property {string} projectPath The path of the project root
+	* @property {stirng} watchPath The path to watch (computed full path of mon_file)
+	* @property {string} mon_root_file The root file to use in each buffer
+	* @property {string} mon_file The file to watch for
+	* @property {string} mon_window_opts Additional +cmd options when spawning the preview window
+	* @property {Chockidar} watcher The Chockidar watcher instance
+	*/
+	var monitors = {};
+
+
+	/**
+	* Default options to use when populating new `monitors` instances
+	*/
+	var monitorDefaults = {
+		mon_root_file: 'package.json',
+		mon_file: '.error',
+		mon_window_opts: '+wincmd\\ J',
+	};
 
 	var watchHandle; // Populated with the Chokidar instance
 
-	var monitorFunc = (path, stats) => {
+	var monitorFunc = (monitor, stats) => {
 		plugin.nvim.outWrite(`Current size ${stats ? stats.size : 'NO STATS'}\n`)
 
 		if (stats && stats.size > 0) {
-			plugin.nvim.command(`pedit ${monitorOpts} ${path}`)
+			plugin.nvim.command(`pedit ${monitor.mon_window_opts} ${monitor.watchPath}`)
 		} else {
 			plugin.nvim.command('pclose');
 		}
 	};
 
-	plugin.registerCommand('MonOn', ()=> Promise.resolve()
-		// Determine projectRoot by scanning this dir upwards until we find a projectRootFile {{{
-		.then(()=> plugin.nvim.callFunction('expand', '%:p'))
-		.then(path => new Promise((resolve, reject) => {
-			var scanPath = path;
+	// MonOn / MonOnLazy {{{
+	var execMonOn = (lazy = false) => Promise.resolve()
+		// Query file path + buffer variables {{{
+		.then(()=> Promise.all([
+			plugin.nvim.callFunction('expand', '%:p'),
+			plugin.nvim.buffer.getVar('mon_root_file').then(res => res || monitorDefaults.mon_root_file),
+			plugin.nvim.buffer.getVar('mon_file').then(res => res || monitorDefaults.mon_file),
+			plugin.nvim.buffer.getVar('mon_window_opts').then(res => res || monitorDefaults.mon_window_opts),
+		]))
+		// }}}
+		// Setup watcher instance {{{
+		.then(data => {
+			var [path, mon_root_file, mon_file, mon_window_opts] = data;
+			if (!path) throw new Error('Cannot determine buffer file path');
+
+			return monitors[path] = {
+				path,
+				projectPath: undefined,
+				watchPath: undefined,
+				mon_root_file,
+				mon_file,
+				mon_window_opts,
+				watcher: undefined,
+			};
+		})
+		// }}}
+		// Determine montitor.projectPath by scanning this dir upwards until we find a monitor.mon_root_file {{{
+		.then(monitor => new Promise((resolve, reject) => {
+			var scanPath = monitor.path;
 
 			var examinePath = ()=>
-				fs.promises.stat(fspath.join(scanPath, projectRootFile))
-					.then(()=> resolve(scanPath))
+				fs.promises.stat(fspath.join(scanPath, monitor.mon_root_file))
+					.then(()=> {
+						monitor.projectPath = scanPath;
+						monitor.watchPath = fspath.join(scanPath, monitor.mon_file);
+						resolve(monitor)
+					})
 					.catch(()=> {
-						if (examinePath == '/') return reject(`Cannot determine project root directory. ${projectRootFile} not found in tree`);
+						if (examinePath == '/') {
+							execMonOff();
+							return reject(`Cannot determine project root directory. ${monitor.mon_root_file} not found in tree`);
+						}
 						scanPath = fspath.resolve(fspath.join(scanPath, '..'));
 						examinePath();
 					})
 
 			examinePath();
 		}))
-		.then(rootDir => projectRoot = rootDir)
 		// }}}
-		.then(()=> plugin.nvim.outWrite(`Monitoring project directory ${projectRoot}\n`))
-		// Setup file watcher which invokes the preview window when we notice the file exists, close when it doesnt {{{
-		.then(()=> watchHandle = chokidar.watch(fspath.join(projectRoot, monitorFile), {
-			alwaysStat: true,
-			awaitWriteFinish: true,
-			disableGlobbing: true,
+		// Display message {{{
+		.then(monitor => {
+			plugin.nvim.outWrite(`Monitoring project directory ${monitor.projectRoot}\n`);
+			return monitor;
 		})
-			.on('change', monitorFunc)
-		)
+		// }}}
+		// Setup file watcher which invokes the preview window when we notice the file exists, close when it doesnt {{{
+		.then(monitor => {
+			monitor.watcher = chokidar.watch(monitor.watchPath, {
+				alwaysStat: true,
+				awaitWriteFinish: true,
+				disableGlobbing: true,
+			})
+				.on('change', (path, stats) => monitorFunc(monitor, stats));
+
+			return monitor;
+		})
 		// }}}
 		// Call the monitor immediately to show the window if its needed {{{
-		.then(()=> fs.promises.stat(fspath.join(projectRoot, monitorFile)))
-		.then(stats => monitorFunc(fspath.join(projectRoot, monitorFile), stats))
+		.then(monitor => fs.promises.stat(monitor.watchPath)
+			.then(stats => monitorFunc(monitor, stats))
+		)
 		// }}}
+		// Error handling {{{
 		.catch(e => {
+			if (lazy) return; // Fail silently if in lazy mode
 			debugger;
 			console.error(e);
-		})
-	, { sync: false });
+		});
+		// }}}
+
+	plugin.registerCommand('MonOn', ()=> execMonOn());
+	plugin.registerCommand('MonOnLazy', ()=> execMonOn(lazy = true));
+	// }}}
+
+	// MonOff {{{
+	var execMonOff = ()=> Promise.resolve()
+		.then(()=> plugin.nvim.callFunction('expand', '%:p'))
+		.then(path => {
+			if (monitors[path]) {
+				monitors[path].watcher.close();
+				delete monitors[path];
+			}
+		});
+
+	plugin.registerCommand('MonOff', execMonOff, {sync: false});
+	// }}}
 };
